9cddfbce9b0689f3a85ebc0166a812f7c985e73f
Muyiwa Olu <muyiwa@hey.com>
2021-01-30 13:04:20 +0000

    Misc: update eslint configuration
    Related: https://github.com/muyiwaolu/simple-shopping-list/pull/3
    
    - Despite what https://github.com/muyiwaolu/simple-shopping-list/pull/3/commits/d3c4a3142fd3ca8d7be039e9b9e66996bc87c2ba says, I didn’t actually add the Airbnb eslint config. This changes this and fixes any issues.
    - Disable import extensions
    - Enforce double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,54 @@
+/* eslint-disable quote-props */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest": true
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest": true,
+  },
+  "extends": [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  "globals": {
+    "JSX": true,
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "react-app",
-        "react-app/jest"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    "ecmaVersion": 12,
+    "project": "./tsconfig.json",
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+  ],
+  "rules": {
+    "@typescript-eslint/member-delimiter-style": ["error", {
+      "multiline": {
+        "delimiter": "semi",
+        "requireLast": true,
+      },
+      "singleline": {
+        "delimiter": "semi",
+        "requireLast": false,
+      },
+    }],
+    "@typescript-eslint/quotes": ["error", "double"],
+    "sort-keys": ["warn", "asc", {
+      "caseSensitive": true,
+      "minKeys": 2,
+      "natural": false,
+    }],
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".ts", ".tsx"],
+      },
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
-        "sort-keys": ["warn", "asc", {
-            "caseSensitive": true,
-            "minKeys": 2,
-            "natural": false,
-        }],
-    },
-    "settings": {
-        "import/resolver": {
-          "node": {
-            "extensions": [".ts", ".tsx"],
-          },
-        },
-    },
+  },
 };
