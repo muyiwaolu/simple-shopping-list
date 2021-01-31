64e3efdeb9898ce52f0b7c127e9c7c424526d99a
Muyiwa Olu <muyiwa@hey.com>
2021-01-31 17:49:18 +0000

    Initial tailwindcss configuration
    Following the tutorial here:
    
    https://tailwindcss.com/docs/guides/create-react-app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{ts,tsx}", "./public/index.html"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
