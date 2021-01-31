358afd9c0909d5981cf5e991efb5848488a0cb4b
Muyiwa Olu <muyiwa@hey.com>
2021-01-31 12:42:05 +0000

    Shopping list accepts user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 
-interface ShoppingListItem {
+interface ShoppingListItemModel {
   id: string;
   name: string;
   completed: boolean;
 }
 
-function createShoppingListItem(name: string): ShoppingListItem {
+type ShoppingListModel = ShoppingListItemModel[];
+
+function createShoppingListItem(name: string): ShoppingListItemModel {
   return {
     id: uuidv4(),
     name,
@@ -16,7 +19,7 @@ function createShoppingListItem(name: string): ShoppingListItem {
     completed: false,
   };
 }
-const SAMPLE_SHOPPING_LIST: ShoppingListItem[] = [
+const SAMPLE_SHOPPING_LIST: ShoppingListModel = [
   createShoppingListItem("eggs"),
   createShoppingListItem("bacon"),
   createShoppingListItem("toilet paper"),
@@ -30,12 +33,12 @@ function ShoppingListItemCompletedIndicator(props: {completed: boolean}): JSX.El
   );
 }
 
-interface ShoppingListItemListItemProps {
+interface ShoppingListItemProps {
   name: string,
   completed: boolean
 }
 
-function ShoppingListItemListItem(props: ShoppingListItemListItemProps): JSX.Element {
+function ShoppingListItem(props: ShoppingListItemProps): JSX.Element {
   const { name, completed } = props;
   return (
     <div>
@@ -46,20 +49,73 @@ function ShoppingListItemListItem(props: ShoppingListItemListItemProps): JSX.Ele
   );
 }
 
+interface CreateShoppingListItemFormProps {
+  shoppingList: ShoppingListModel;
+  setShoppingList: (shoppingList: ShoppingListModel) => void;
+}
+
+function CreateShoppingListItemForm(props: CreateShoppingListItemFormProps): JSX.Element {
+  const { shoppingList, setShoppingList } = props;
+
+  const [shoppingListItemName, setShoppingListItemName] = useState("");
+
+  const inputId = "create-shopping-list-item-input";
+  return (
+    <form>
+      <label htmlFor={inputId}>
+        Item name
+        <input
+          id={inputId}
+          type="text"
+          value={shoppingListItemName}
+          onChange={(event) => {
+            setShoppingListItemName(event.target.value);
+          }}
+        />
+      </label>
+      <button
+        type="button"
+        onClick={() => {
+          const newShoppingListItem = createShoppingListItem(shoppingListItemName);
+          const newShoppingList = [...shoppingList, newShoppingListItem];
+
+          setShoppingList(newShoppingList);
+        }}
+      >
+        Add item
+      </button>
+    </form>
+  );
+}
+
+interface ShoppingListProps {
+  shoppingList: ShoppingListModel
+}
+
+function ShoppingList(props: ShoppingListProps): JSX.Element {
+  const { shoppingList } = props;
+  return (
+    <div>
+      {shoppingList.map(({ id, name, completed }) => (
+        <ShoppingListItem
+          key={id}
+          name={name}
+          completed={completed}
+        />
+      ))}
+    </div>
+  );
+}
+
 function App(): JSX.Element {
+  const [shoppingList, setShoppingList] = useState(SAMPLE_SHOPPING_LIST);
   return (
     <div className="App">
-      <div>
-        {
-          SAMPLE_SHOPPING_LIST.map(({ id, name, completed }) => (
-            <ShoppingListItemListItem
-              key={id}
-              name={name}
-              completed={completed}
-            />
-          ))
-        }
-      </div>
+      <ShoppingList shoppingList={shoppingList} />
+      <CreateShoppingListItemForm
+        shoppingList={shoppingList}
+        setShoppingList={setShoppingList}
+      />
     </div>
   );
 }
