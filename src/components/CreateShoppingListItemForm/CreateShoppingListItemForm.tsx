b798e1f5c149ea60863cf2a0a7d16f0178251f69
Muyiwa Olu <muyiwa@hey.com>
2021-02-06 13:11:29 +0000

    Small bug fixes
    - Border colour correctly changes to red in dark mode when input is invalid
    - Move error text span to the label DOM element

--- src/components/CreateShoppingListItemForm/CreateShoppingListItemForm.tsx
+++ src/components/CreateShoppingListItemForm/CreateShoppingListItemForm.tsx
@@ -22,8 +22,8 @@ export default function CreateShoppingListItemForm(
     setUserAttemptedToSubmitInvalidForm(false);
   }, []);
 
-  const validShoppingListItem = () => shoppingListItemName.length > 0
-     && shoppingListItemName.length <= 30;
+  const validShoppingListItem = (nameToCheck = shoppingListItemName) => nameToCheck.length > 0
+     && nameToCheck.length <= 30;
 
   const addShoppingListItem = () => {
     if (!validShoppingListItem()) {
@@ -52,16 +52,17 @@ export default function CreateShoppingListItemForm(
           name={INPUT_ID}
           className={`
             border mr-2 rounded-md p-2 dark:bg-black
-            dark:placeholder-white dark:border-green-700
-            ${userAttemptedToSubmitInvalidForm ? "border-red-500" : ""}
+            dark:placeholder-white
+            ${userAttemptedToSubmitInvalidForm ? "border-red-500 dark:border-red-500" : "dark:border-green-700"}
           `}
           data-testid={INPUT_ID}
           type="text"
           value={shoppingListItemName}
           placeholder="eggs 🍳"
           onChange={(event) => {
-            setShoppingListItemName(event.target.value);
-            if (validShoppingListItem()) {
+            const newShoppingListItemName = event.target.value;
+            setShoppingListItemName(newShoppingListItemName);
+            if (validShoppingListItem(newShoppingListItemName)) {
               setUserAttemptedToSubmitInvalidForm(false);
             }
           }}
@@ -71,15 +72,16 @@ export default function CreateShoppingListItemForm(
             }
           }}
         />
+        {userAttemptedToSubmitInvalidForm && (
+        <span className="text-xs text-red-500">
+          Item name must be between 1 and 30 characters.
+        </span>
+        )}
       </label>
-      {userAttemptedToSubmitInvalidForm && (
-      <span className="text-xs text-red-500">
-        Item name must be between 1 and 30 characters.
-      </span>
-      )}
+
       <button
         type="button"
-        className="bg-green-700 rounded-md shadow p-2 my-5 text-white disabled:bg-gray-700"
+        className="bg-green-700 rounded-md shadow text-white disabled:bg-gray-700 p-2 w-32 my-2"
         data-testid={BUTTON_ID}
         onClick={() => {
           if (!validShoppingListItem()) {
